0cb2e238160770790522d8cb35c43afcac61f8e3
Tarun <tarun.karmakar.dev@gmail.com>
2021-01-21 11:33:24 +0530

    seperated backend to different repository tasks_api

--- tasks_api/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('tasks/', views.tasks, name="display_tasks"),
-    path('tasks/<int:id>', views.tasks, name="display_tasks")
-]
